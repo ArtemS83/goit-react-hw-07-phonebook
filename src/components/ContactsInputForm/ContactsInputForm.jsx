617815135c7cfa4865e9d2d8e1a2c99e2fd0c498
Atrem Sergieiev <a_serg83@ukr.net>
2021-04-01 12:49:34 +0200

    start

--- src/components/ContactsInputForm/ContactsInputForm.jsx
+++ src/components/ContactsInputForm/ContactsInputForm.jsx
@@ -60,6 +60,9 @@ const ContactsInputForm = () => {
           type="text"
           name="name"
           value={name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
           placeholder="Enter contact"
           onChange={handleInputChange}
         />
@@ -70,6 +73,9 @@ const ContactsInputForm = () => {
           type="tel"
           name="number"
           value={number}
+          pattern="(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})"
+          title="Номер телефона должен состоять из 11-12 цифр и может содержать цифры, пробелы, тире, пузатые скобки и может начинаться с +"
+          required
           placeholder="Enter number phone"
           onChange={handleInputChange}
         />
